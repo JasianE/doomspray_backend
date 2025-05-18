f29295ed89a3156c33305bb1179abe55eb9f9086
jun li <junchenli423@gmail.com>
2025-05-17 21:20:02 -0400

    done the chrome

--- app.js
+++ app.js
@@ -8,24 +8,18 @@ const { ReadlineParser } = require('@serialport/parser-readline');
 
 const app = express();
 app.use(cors())
+app.use(express.json());
 
 let total_distractions = 0;
 const PORT = process.env.PORT || 5000;
-
-/*const port = new SerialPort({
+/*
+const port = new SerialPort({
   path: '/dev/ttyACM0', 
   baudRate: 9600,
 });
 
 const parser = port.pipe(new ReadlineParser({ delimiter: '\n' }));
-// Read the 
-port.on("open", () => {
-  console.log('serial port open');
-  setTimeout(() => {
-    port.write('push\n');
-    console.log('Sent push command to Arduino');
-  }, 2000); // wait 2 seconds
-});
+
 parser.on('data', data =>{
   console.log('got word from arduino:', data);
 });
@@ -36,26 +30,34 @@ app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
 
-app.get("/scrape", async (req, res) => {
-    const url = req.query.url;
+app.post("/scrape", async (req, res) => {
+    console.log('hello!')
+    const data = req.body;
+    const extra_urls = req.body.extra_urls; // The urls that the user has blocked themselves
+    const the_url = req.body.the_url; // The url that the user is currently accessing 
+    
+    let isTheWebsiteDistracting = false; // Changes to true value if the url user is accesing is in extra_urls
     try {
-      const response = await axios.get(url);
-      const html = response.data;
-      const $ = cheerio.load(html);
-      const data = [];
-      $("a").each((index, element) => {
-        data.push({
-          text: $(element).text(),
-          href: $(element).attr("href"),
-        });
-      });
+      const response = await axios.get(the_url);
+      const html = response.data; // parse the html and load it into cheerio
+      const $ = cheerio.load(html); 
       $('video').each((index, element) => {
-        total_distractions = total_distractions + 2; //make videos worth 2 times
+        total_distractions = total_distractions + 30; //make videos worth 10 times
       })
-      
-      res.json(data)
-      if(total_distractions > 5){
+      $('img').each((index, element) => {
+        total_distractions = total_distractions+1
+      })
+
+      for(let i = 0; i < extra_urls.length; i++){
+        if(extra_urls[i] == the_url){
+          isTheWebsiteDistracting = true;
+        }
+      }
+
+      if(total_distractions >30 || isTheWebsiteDistracting){
         res.send('Distracting')
+
+        //port.write('push\n')
       } else {
         res.send('Good')
       }
